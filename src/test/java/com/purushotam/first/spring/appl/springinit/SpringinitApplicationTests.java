6a49246b673bb1c503755b7eb69469676660f547
Purushotam <purushotamsingh2001@gmail.com>
2024-10-30 15:30:08 +0530

    All files added

---
+++ src/test/java/com/purushotam/first/spring/appl/springinit/SpringinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purushotam.first.spring.appl.springinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
